d9b5fe85ea36e357c4f4058ad5f57d47795e9e4a
GABrambila <brambila.gabriel26@gmail.com>
2024-02-21 21:02:14 -0300

    aula21-02

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 $(document).ready(function(){
-  
+
+    const baseImagens = {
+        
+        Todas: ['Todas.jpg', 'Todas1.jpg', 'Todas2.jpg', 'Todas3.jpg'],
+        Animais: ['Animal.jpg', 'Animal1.jpg', 'Animal2.jpg', 'Animal3.jpg'],
+        Cidade: ['Cidade.jpg', 'Cidade1.jpg', 'Cidade2.jpg', 'Cidade3.jpg'],
+        Natureza: ['Natureza.jpg', 'Natureza1.jpg', 'Natureza2.jpg', 'Natureza3.jpg'],
+
+
+    }
+
+       function carregarImagens(categoria){
+        const imagens = baseImagens[categoria]; 
+        const boxImagens = $('body').find('.box-imagens');
+
+        imagens.forEach(img => {
+            console.log(img); 
+            boxImagens.append('<div> <img src ="imagens/'+img+'" /> </div>')
+            
+
+        });
+
+         
+       }
+
     $('.botao-categoria').on('click', function(){
+        
        alert($(this).data('categoria'));
-    });
+
+       $('body').find('.botao-categoria').removeClass('active'); 
+
+       $(this).addClass('active'); 
+
+       const categoria = $(this).data('categoria'); 
+       carregarImagens(categoria); 
+       });
 
 
 
